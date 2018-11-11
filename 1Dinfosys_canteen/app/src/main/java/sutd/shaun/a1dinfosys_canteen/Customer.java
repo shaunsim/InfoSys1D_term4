6ae6269a3c99df1409a0e367cf8bd32b1e303084
zddd-redred <dan_zhu@mymail.sutd.edu.sg>
2018-11-11 19:42:50 +0800

    Update 1Dinfosys_canteen/app/src/main/java/sutd/shaun/a1dinfosys_canteen/Customer.java

--- 1Dinfosys_canteen/app/src/main/java/sutd/shaun/a1dinfosys_canteen/Customer.java
+++ 1Dinfosys_canteen/app/src/main/java/sutd/shaun/a1dinfosys_canteen/Customer.java
@@ -9,36 +9,64 @@ public class Customer {
     private ArrayList<Food> shoppingCart;
     private boolean alert = false;
 
+    //constructor
     Customer(){
     }
-    Customer(String id){
+
+    public String getId(){
+        return this.id;
+    }
+    public void setId(String id){
         this.id = id;
     }
-    Customer(ArrayList<Bill> history){
+
+
+    public ArrayList<Bill> getHistory(){
+        return this.history;
+    }
+    public void setHistory(ArrayList<Bill>){
         this.history = history;
     }
-    Customer(double EWallet){
+
+
+    public double getEWallet(){
+        return this.EWallet;
+    }
+    public void setEWallet(double EWallet){
         this.EWallet = EWallet;
     }
-    Customer(ArrayList<Food> shoppingCart){
+
+
+    public ArrayList<Food> getShoppingCart(){
         this.shoppingCart = shoppingCart;
     }
-    Customer(boolean alert){
+    public void setShoppingCart(ArrayList<Food> shoppingCart){
+        this.shoppingCart = shoppingCart;
+    }
+
+
+    public boolean getAlert(){
+        return this.alert;
+    }
+    public void setAlert(boolean alert){
         this.alert = alert;
     }
 
-    public void receiveNotification(String id){
-        if(this.id==id){
-            this.alert = true;
-        }
 
+    //methods:
+    public void receiveNotification(String id){
+        //TO DO: observer/subscriber, if status changes for a food item,
+        //set alert to be true (to activate food buzzer in the app)
+        setAlert(boolean True);
     }
+
 
     public void sendOrder(ArrayList<Food> shoppingCart){
         //TO DO:
         //push the shopping cart info to the server
 
     }
+
 
     public String pay(){
         //TO DO: calculate $$ to be paid (double total)
@@ -63,13 +91,18 @@ public class Customer {
     public ArrayList<Food> removeFood(Food food){
         if(this.shoppingCart.contains(food)){
             this.shoppingCart.remove(food);
+        }else{
+            //TO DO: print an error message that the food cannot be removed as
+            //it is not in the shopping cart
         }
         return this.shoppingCart;
     }
 
+
     public void viewMenu(ArrayList<Food> menu){
         //TO DO: display menu on the app
     }
+
 
     public void sendAlert(boolean alert){
         if(this.alert==true){
